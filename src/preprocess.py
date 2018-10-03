abf0515d6ddad7a71c3e7a33a539bb657667752d
Rolando Garcia <rogarcia@berkeley.edu>
2018-10-03 10:43:09 -0700

    update

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,13 +1,32 @@
+# General Imports
 import flor
 import json
 import pandas as pd
+import scipy.sparse
 import numpy as np
 import time
 
+# Imports for Pre-Processing
 from stop_words import get_stop_words
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from nltk.stem.porter import PorterStemmer
+
+# Imports for train/test Split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+
+# Imports for Training and Testing
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import average_precision_score, recall_score, precision_recall_fscore_support
 
 @flor.func
 def preprocessing(data_loc, intermediate_X, intermediate_y, **kwargs):
@@ -16,64 +35,64 @@ def preprocessing(data_loc, intermediate_X, intermediate_y, **kwargs):
     Data Preprocessing
 
     '''
+    import random
     print("Data Preprocessing")
-#     data = pd.read_json(data_loc)
-#     X = data['text']
-#     y = data['rating'].astype(np.float64)
+    data = pd.read_json(data_loc)
+    X = data['text']
+    y = data['rating'].astype(np.float64)
     
-#     en_stop = get_stop_words('en')
+    en_stop = get_stop_words('en')
 
-#     def filter_sentence(el):
-#         tokens = word_tokenize(el)
-#         tokens = [word for word in tokens if word.isalpha()]
-#         tokens = [word for word in tokens if word not in en_stop]
-#         tokens = stem_words(tokens)
-#         tokens = lemma_words(tokens)
+    def filter_sentence(el):
+        tokens = word_tokenize(el)
+        tokens = [word for word in tokens if word.isalpha()]
+        tokens = [word for word in tokens if word not in en_stop]
+        tokens = stem_words(tokens)
+        tokens = lemma_words(tokens)
 
-#         ret_str = " ".join(tokens) 
+        ret_str = " ".join(tokens) 
 
-#         return ret_str 
+        return ret_str 
 
 
-#     #Credit to https://www.kdnuggets.com/2018/03/text-data-preprocessing-walkthrough-python.html
-#     #for stem_words and lemma_words
-#     def stem_words(words):
-#         stemmer = PorterStemmer()
-#         stems = []
-#         for word in words:
-#             stem = stemmer.stem(word)
-#             stems.append(stem)
-#         return stems
+    #Credit to https://www.kdnuggets.com/2018/03/text-data-preprocessing-walkthrough-python.html
+    #for stem_words and lemma_words
+    def stem_words(words):
+        stemmer = PorterStemmer()
+        stems = []
+        for word in words:
+            stem = stemmer.stem(word)
+            stems.append(stem)
+        return stems
 
-#     def lemma_words(words):
-#         lemmatizer = WordNetLemmatizer()
-#         lemmas = []
-#         for word in words:
-#             lemma = lemmatizer.lemmatize(word, pos='v')
-#             lemmas.append(lemma)
-#         return lemmas
+    def lemma_words(words):
+        lemmatizer = WordNetLemmatizer()
+        lemmas = []
+        for word in words:
+            lemma = lemmatizer.lemmatize(word, pos='v')
+            lemmas.append(lemma)
+        return lemmas
 
-#     start_time = time.time()
-#     X = [filter_sentence(el) for el in X]
-#     print("--- %s seconds ---" % (time.time() - start_time))
+    def train_drop(el):
+        tokens = word_tokenize(el)
+        tokens = [el for el in tokens if random.random() >= 0.75]
+        ret_str = " ".join(tokens) 
+        return ret_str 
 
-#     y_new = []
-#     for el in y:
-#         ret = 0
-#         if el <= 5:
-#             ret = 0
-#         else:
-#             ret = 1
-#         y_new.append(ret)
-#     y = y_new
+    start_time = time.time()
+    X = [filter_sentence(el) for el in X]
+#     X = [train_drop(el) for el in X]
+    print("--- %s seconds ---" % (time.time() - start_time))
 
-    # Load the cleaned data
-    with open('data_clean_X.json') as json_data:
-        X = json.load(json_data)
-        json_data.close()
-    with open('data_clean_y.json') as json_data:
-        y = json.load(json_data)
-        json_data.close()
+    y_new = []
+    for el in y:
+        ret = 0
+        if el <= 5:
+            ret = 0
+        else:
+            ret = 1
+        y_new.append(ret)
+    y = y_new
 
     with open(intermediate_X, 'w') as outfile:
        json.dump(X, outfile)
--- src/tutorial/florfunctions.py
+++ src/tutorial/florfunctions.py
@@ -6,12 +6,6 @@ import scipy.sparse
 import numpy as np
 import time
 
-# Imports for Pre-Processing
-from stop_words import get_stop_words
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-from nltk.stem.porter import PorterStemmer
-
 # Imports for train/test Split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
@@ -27,88 +21,9 @@ from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.metrics import average_precision_score, recall_score, precision_recall_fscore_support
+    
 
 @flor.func
-def preprocessing(data_loc, intermediate_X, intermediate_y, **kwargs):
-    '''
-
-    Data Preprocessing
-
-    '''
-    import random
-    print("Data Preprocessing")
-    data = pd.read_json(data_loc)
-    X = data['text']
-    y = data['rating'].astype(np.float64)
-    
-    en_stop = get_stop_words('en')
-
-    def filter_sentence(el):
-        tokens = word_tokenize(el)
-        tokens = [word for word in tokens if word.isalpha()]
-        tokens = [word for word in tokens if word not in en_stop]
-        tokens = stem_words(tokens)
-        tokens = lemma_words(tokens)
-
-        ret_str = " ".join(tokens) 
-
-        return ret_str 
-
-
-    #Credit to https://www.kdnuggets.com/2018/03/text-data-preprocessing-walkthrough-python.html
-    #for stem_words and lemma_words
-    def stem_words(words):
-        stemmer = PorterStemmer()
-        stems = []
-        for word in words:
-            stem = stemmer.stem(word)
-            stems.append(stem)
-        return stems
-
-    def lemma_words(words):
-        lemmatizer = WordNetLemmatizer()
-        lemmas = []
-        for word in words:
-            lemma = lemmatizer.lemmatize(word, pos='v')
-            lemmas.append(lemma)
-        return lemmas
-
-    def train_drop(el):
-        tokens = word_tokenize(el)
-        tokens = [el for el in tokens if random.random() >= 0.75]
-        ret_str = " ".join(tokens) 
-        return ret_str 
-
-    start_time = time.time()
-    X = [filter_sentence(el) for el in X]
-#     X = [train_drop(el) for el in X]
-    print("--- %s seconds ---" % (time.time() - start_time))
-
-    y_new = []
-    for el in y:
-        ret = 0
-        if el <= 5:
-            ret = 0
-        else:
-            ret = 1
-        y_new.append(ret)
-    y = y_new
-
-    # Load the cleaned data
-#     with open('data_clean_X.json') as json_data:
-#         X = json.load(json_data)
-#         json_data.close()
-#     with open('data_clean_y.json') as json_data:
-#         y = json.load(json_data)
-#         json_data.close()
-
-    with open(intermediate_X, 'w') as outfile:
-       json.dump(X, outfile)
-    with open(intermediate_y, 'w') as outfile:
-       json.dump(y, outfile)
-    
-
-@flor.func #SOLUTION
 def traintest_split(intermediate_X, intermediate_y, X_train, X_test, y_train, y_test, **kwargs):
     '''
 
@@ -123,7 +38,6 @@ def traintest_split(intermediate_X, intermediate_y, X_train, X_test, y_train, y_
         json_data.close()
     X_tr, X_te, y_tr, y_te = train_test_split(X, y, test_size=0.20, random_state=92)
 
-    # X_tr = [train_drop(el) for el in X_tr]
     vectorizer = TfidfVectorizer()
     start_time = time.time()
     vectorizer.fit(X_tr)
