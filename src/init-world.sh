0003e608ae23c6af1097bed08708e77d9dfce1fa
Rolando Garcia <rogarcia@berkeley.edu>
2018-10-10 15:21:42 -0700

    minor patch

--- src/init-world.sh
+++ src/init-world.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-git clone git@github.com:ucbrise/flor-camp2018.git /tmp/flor-camp2018
-cp -a /tmp/flor-camp2018/src/tutorial/. /home/$NB_USER/tutorial/
+echo "hello world"
+#git clone https://github.com/ucbrise/flor-camp2018.git /tmp/flor-camp2018
+#cp -a /tmp/flor-camp2018/src/tutorial/. /home/$NB_USER/tutorial/
